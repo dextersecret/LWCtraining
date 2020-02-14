1ddb0dd2eb2e6f6198930e20d7672fb412555ed9
dextersecret <vlad.mironov@concretecanvas.com>
2020-02-14 16:03:53 +0000

    lwc5 deployed

---
+++ force-app/main/default/lwc/lwc5ListItem/lwc5ListItem.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Lwc5ListItem extends LightningElement {
+	@api item = '';
+}
---
+++ force-app/main/default/lwc/lwc5app/lwc5app.js
@@ -0,0 +1,5 @@
+import { LightningElement, track } from 'lwc';
+
+export default class Lwc5app extends LightningElement {
+	@track list = [ 'Apple', 'Banana', 'Clementine' ];
+}
