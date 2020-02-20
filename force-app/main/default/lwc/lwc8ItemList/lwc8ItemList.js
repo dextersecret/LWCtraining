6473c38b16372b8fe6434495e318e28637af4eee
dextersecret <vlad.mironov@concretecanvas.com>
2020-02-20 10:34:36 +0000

    lwc8

---
+++ force-app/main/default/lwc/lwc8ItemList/lwc8ItemList.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Lwc8ItemList extends LightningElement {}
---
+++ force-app/main/default/lwc/lwc8app/lwc8app.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Lwc8app extends LightningElement {}
